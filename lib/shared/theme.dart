b6584d6d7e58711296eafea01dbc0a3712ea95e4
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-06-10 21:07:00 +0700

    update theme.dart

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -3,13 +3,20 @@ import 'package:google_fonts/google_fonts.dart';
 
 double defaultMargin = 30.0;
 
-// COLORS
-Color primaryColorGrey = const Color(0xff7B7878);
-Color primaryColorNobel = const Color(0xff969595);
-Color primaryColorSilver = const Color(0xffC4C4C4);
-Color primaryColorWhisper = const Color(0xffE5E5E5);
+// COLORS WIREFRRAME
+Color primaryColorGrey = const Color(0xff7B7878); // Container WF
+Color primaryColorNobel = const Color(0xff969595); // Photo WF
+Color primaryColorSilver = const Color(0xffC4C4C4); // Icons WF
+Color primaryColorWhisper = const Color(0xffE5E5E5); // Text WF
 Color primaryColorWhite = const Color(0xffFFFFFF);
 Color primaryColorBlack = const Color(0xff1E1E1E);
+
+Color primaryColorWhiteSmoke = const Color(0xffF2F2F2); // color1
+Color primaryColorMidnightExpress = const Color(0xff242831); // color2
+Color primaryColorBlackRussian = const Color(0xff10141D); // color3
+Color secondaryColorTengerineYellow = const Color(0xffFEC901); // color4
+Color secondaryColorWhite = const Color(0xffFFFFFF); // color5
+Color secondaryColorMischka = const Color(0xff9EA1AC); // color6
 
 // FONTS
 

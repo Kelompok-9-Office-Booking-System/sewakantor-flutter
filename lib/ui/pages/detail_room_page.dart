96127a2da2bb1b02f9fe8907b805d192ea3dde48
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-07-11 16:16:49 +0700

    add filter search schedule tour

--- lib/ui/pages/detail_room_page.dart
+++ lib/ui/pages/detail_room_page.dart
@@ -5,8 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:sewakantor_flutter/models/space_model.dart';
 import 'package:sewakantor_flutter/shared/theme.dart';
 import 'package:sewakantor_flutter/ui/pages/home/chat_page.dart';
+import 'package:sewakantor_flutter/ui/widgets/custom_button_rectangle_icon.dart';
+import 'package:sewakantor_flutter/ui/widgets/custom_button_select.dart';
+import 'package:sewakantor_flutter/ui/widgets/custom_button_text.dart';
 import 'package:sewakantor_flutter/ui/widgets/custom_card.dart';
 import 'package:sewakantor_flutter/ui/widgets/custom_card_reviews.dart';
+import 'package:sewakantor_flutter/ui/widgets/custom_text_form_field.dart';
 import 'package:sewakantor_flutter/ui/widgets/custom_widget_facilities.dart';
 import 'package:sewakantor_flutter/ui/widgets/custom_widget_nearby_places.dart';
 import 'package:sewakantor_flutter/ui/widgets/custom_widget_office_type.dart';
@@ -320,6 +324,341 @@ class DetailRoomPage extends StatelessWidget {
       );
     }
 
+    Future<void> showFilterSearchScheduleTour() async {
+      // print(dateRange);
+
+      return showDialog(
+          context: context,
+          builder: (context) {
+            DateTimeRange dateRange = DateTimeRange(
+              start: DateTime(2022, 11, 5),
+              end: DateTime(2022, 12, 24),
+            );
+
+            // Future pickDateRange() async {
+
+            //       if (newDateRange == null) return;
+
+            //       setStateDialog(() {
+            //         dateRange = newDateRange;
+            //       });
+            //     }
+
+            String tanggal_mulai = 'dd/mm/yyyy';
+            String tanggal_berakhir = 'dd/mm/yyyy';
+
+            int amountOfPeople = 1;
+
+            RangeValues valuesCapacity = RangeValues(0, 1000);
+            RangeValues valuesPrice = RangeValues(0, 10000000);
+
+            List<String> officeTypeList = [
+              'Office Room',
+              'Coworking',
+              'Virtual Room',
+              'Meeting Room',
+            ];
+
+            return Container(
+              // margin: EdgeInsets.all(20),
+              width: double.infinity,
+              child: Dialog(
+                insetPadding: EdgeInsets.all(10),
+                backgroundColor: primaryColorMidnightExpress,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: StatefulBuilder(builder: (context, setState) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      padding: EdgeInsets.all(35),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  'Schedule Tour',
+                                  style: primaryTextStyle.copyWith(
+                                    color: primaryColorWhite,
+                                    fontWeight: semiBold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Icon(
+                                    Icons.close,
+                                    color: primaryColorWhite,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          CustomTextFormField(
+                            title: 'Name',
+                            colorTitle: primaryColorWhite,
+                            colorBorder: primaryColorWhite,
+                            colorHintText: primaryColorWhite.withOpacity(0.4),
+                            hintText: 'Enter your name',
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          CustomTextFormField(
+                            title: 'Name Company',
+                            colorTitle: primaryColorWhite,
+                            colorBorder: primaryColorWhite,
+                            colorHintText: primaryColorWhite.withOpacity(0.4),
+                            hintText: 'Enter your name company',
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          CustomTextFormField(
+                            title: 'Email Company',
+                            colorTitle: primaryColorWhite,
+                            colorBorder: primaryColorWhite,
+                            colorHintText: primaryColorWhite.withOpacity(0.4),
+                            hintText: 'Enter your email company',
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          CustomTextFormField(
+                            title: 'Phone Number',
+                            colorTitle: primaryColorWhite,
+                            colorBorder: primaryColorWhite,
+                            colorHintText: primaryColorWhite.withOpacity(0.4),
+                            hintText: 'Enter your phone number',
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            'Select Date',
+                            style: primaryTextStyle.copyWith(
+                              color: primaryColorWhite,
+                              fontWeight: semiBold,
+                              fontSize: 14,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'From',
+                                    style: primaryTextStyle.copyWith(
+                                      color: primaryColorWhite,
+                                      fontWeight: medium,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  CustomButtonSelect(
+                                    onTap: () async {
+                                      DateTimeRange? newDateRange =
+                                          await showDateRangePicker(
+                                        context: context,
+                                        firstDate: DateTime(1900),
+                                        lastDate: DateTime(2100),
+                                      );
+                                      if (newDateRange != null) {
+                                        setState(() {
+                                          tanggal_mulai =
+                                              '${newDateRange.start.day}/${newDateRange.start.month}/${newDateRange.start.year}';
+                                          tanggal_berakhir =
+                                              '${newDateRange.end.day}/${newDateRange.end.month}/${newDateRange.end.year}';
+                                        });
+                                      }
+                                    },
+                                    date: '${tanggal_mulai}',
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'To',
+                                    style: primaryTextStyle.copyWith(
+                                      color: primaryColorWhite,
+                                      fontWeight: medium,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  CustomButtonSelect(
+                                    onTap: () async {
+                                      DateTimeRange? newDateRange =
+                                          await showDateRangePicker(
+                                        context: context,
+                                        firstDate: DateTime(1900),
+                                        lastDate: DateTime(2100),
+                                      );
+                                      if (newDateRange != null) {
+                                        setState(() {
+                                          tanggal_mulai =
+                                              '${newDateRange.start.day}/${newDateRange.start.month}/${newDateRange.start.year}';
+                                          tanggal_berakhir =
+                                              '${newDateRange.end.day}/${newDateRange.end.month}/${newDateRange.end.year}';
+                                        });
+                                      }
+                                    },
+                                    date: '${tanggal_berakhir}',
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            'Select Time',
+                            style: primaryTextStyle.copyWith(
+                              color: primaryColorWhite,
+                              fontWeight: semiBold,
+                              fontSize: 14,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CustomButtonSelect(
+                                onTap: () async {},
+                                date: '10.00 AM',
+                              ),
+                              CustomButtonSelect(
+                                onTap: () async {},
+                                date: 'Duration',
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Space Type',
+                                    style: primaryTextStyle.copyWith(
+                                      color: primaryColorWhite,
+                                      fontWeight: semiBold,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  CustomButtonSelect(
+                                    onTap: () async {},
+                                    date: 'Meeting Room',
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Amount of People',
+                                    style: primaryTextStyle.copyWith(
+                                      color: primaryColorWhite,
+                                      fontWeight: semiBold,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      CustomButtonRectangleIcon(
+                                        icon: Icons.remove,
+                                        bgColor: primaryColorBlackRussian,
+                                        color: secondaryColorMischka,
+                                        onTap: () {
+                                          setState(
+                                            () {
+                                              amountOfPeople -= 1;
+                                            },
+                                          );
+                                        },
+                                      ),
+                                      SizedBox(
+                                        width: 16,
+                                      ),
+                                      Text(
+                                        '${amountOfPeople}',
+                                        style: primaryTextStyle.copyWith(
+                                          color: primaryColorWhite,
+                                          fontWeight: semiBold,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 16,
+                                      ),
+                                      CustomButtonRectangleIcon(
+                                        icon: Icons.add,
+                                        bgColor: secondaryColorMischka,
+                                        color: primaryColorBlackRussian,
+                                        onTap: () {
+                                          setState(
+                                            () {
+                                              amountOfPeople += 1;
+                                            },
+                                          );
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          CustomButtonText(
+                            text: 'Submit',
+                            bgColor: primaryColorBlackRussian,
+                            onPrressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            );
+          });
+    }
+
     Widget customBottomNav() {
       return ClipRRect(
         borderRadius: BorderRadius.vertical(
@@ -334,7 +673,7 @@ class DetailRoomPage extends StatelessWidget {
             onTap: (value) {
               print(value);
               if (value == 0) {
-                showAboutDialog(context: context);
+                showFilterSearchScheduleTour();
               } else if (value == 1) {
                 Navigator.pushNamed(
                   context,
